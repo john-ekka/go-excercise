569f220e1ca42c57fd7ff3db87abeda980c76f90
JE <john.ekka@gamil.com>
2023-04-09 20:14:28 -0500

    completed excercise-7,8

---
+++ excercise-7.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Create an array with the number 0 to 10
+
+	var numbers = []int64{}
+
+	fmt.Println((numbers))
+
+	//Create an array of strings with names
+	countires := []string{}
+
+	fmt.Println((countires))
+
+	var arrKeyValue = [5]string{3: "Chris", 4: "Ron"}
+
+	fmt.Println((arrKeyValue[8]))
+
+}
---
+++ excercise-8.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+	//Make a program that counts from 1 to 10.
+	for i := 1; i <= 10; i++ {
+
+		println(i)
+
+	}
+
+}
